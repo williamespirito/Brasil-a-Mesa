5978808ba2707297c01b5edb0b0da241b9eae5dc
williamespirito <williammenezes76@gmail.com>
2023-07-06 09:32:28 -0300

    configuração inicial

---
+++ brasil_a_mesa/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'brasil_a_mesa',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
